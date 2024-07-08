9362d7a14cf353f7dac417016da246b699a9a0f2
RuanCardoso <cardoso.ruan050322@gmail.com>
2024-07-08 03:08:57 -0400

    Added default objects if null

--- Core/Shared/Components/NetworkComponentService.cs
+++ Core/Shared/Components/NetworkComponentService.cs
@@ -1,6 +1,8 @@
 using System;
 using Omni.Core.Interfaces;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Omni.Core.Components
 {
@@ -8,10 +10,10 @@ namespace Omni.Core.Components
     public class NetworkComponentService : ServiceBehaviour, INetworkComponentService
     {
         [SerializeField]
-        private MonoBehaviour m_Component;
+        private Component m_Component;
 
+        public Component Component => m_Component;
         public GameObject GameObject => gameObject;
-        public MonoBehaviour Component => m_Component;
 
         protected override void OnAwake()
         {
@@ -28,6 +30,57 @@ namespace Omni.Core.Components
             if (string.IsNullOrEmpty(ServiceName))
             {
                 ServiceName = GameObject.name;
+            }
+
+            if (m_Component == null)
+            {
+                if (TryGetComponent<Button>(out var mButton))
+                {
+                    m_Component = mButton;
+                }
+                else if (TryGetComponent<TMP_Text>(out var mText))
+                {
+                    m_Component = mText;
+                }
+                else if (TryGetComponent<TMP_InputField>(out var mInputField))
+                {
+                    m_Component = mInputField;
+                }
+                else if (TryGetComponent<TMP_Dropdown>(out var mDropdown))
+                {
+                    m_Component = mDropdown;
+                }
+                else if (TryGetComponent<Text>(out var mUnityText))
+                {
+                    m_Component = mUnityText;
+                }
+                else if (TryGetComponent<InputField>(out var mUnityInputField))
+                {
+                    m_Component = mUnityInputField;
+                }
+                else if (TryGetComponent<Dropdown>(out var mUnityDropdown))
+                {
+                    m_Component = mUnityDropdown;
+                }
+                else if (TryGetComponent<Toggle>(out var mUnityToggle))
+                {
+                    m_Component = mUnityToggle;
+                }
+                else if (TryGetComponent<Slider>(out var mUnitySlider))
+                {
+                    m_Component = mUnitySlider;
+                }
+                else if (TryGetComponent<Canvas>(out var mCanvas))
+                {
+                    m_Component = mCanvas;
+                }
+                else
+                {
+                    if (TryGetComponent<Image>(out var mImage))
+                    {
+                        m_Component = mImage;
+                    }
+                }
             }
         }
     }
