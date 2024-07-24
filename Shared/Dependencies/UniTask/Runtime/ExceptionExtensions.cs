9d9e09ab58480f11930124c66784cea8c6c164ec
RuanCardoso <cardoso.ruan050322@gmail.com>
2024-07-24 02:36:49 -0400

    addedmoved dependencies.
    added dottween.
    moved unitask to shared.
    added ingamedebugconsole.

---
+++ Shared/Dependencies/UniTask/Runtime/ExceptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Omni.Threading.Tasks
+{
+    public static class ExceptionExtensions
+    {
+        public static bool IsOperationCanceledException(this Exception exception)
+        {
+            return exception is OperationCanceledException;
+        }
+    }
+}
+
---
+++ Shared/Dependencies/UniTask/Runtime/Linq/AsUniTaskAsyncEnumerable.cs
@@ -0,0 +1,10 @@
+﻿namespace Omni.Threading.Tasks.Linq
+{
+    public static partial class UniTaskAsyncEnumerable
+    {
+        public static IUniTaskAsyncEnumerable<TSource> AsUniTaskAsyncEnumerable<TSource>(this IUniTaskAsyncEnumerable<TSource> source)
+        {
+            return source;
+        }
+    }
+}
