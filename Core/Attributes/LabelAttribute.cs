f19de254c76af0b894cf21aaf289cebcf286b297
RuanCardoso <cardoso.ruan050322@gmail.com>
2024-07-25 21:21:17 -0400

    Added missing [AttributeUsage]

--- Core/Attributes/LabelAttribute.cs
+++ Core/Attributes/LabelAttribute.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 namespace Omni.Core.Attributes
 {
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
     public class LabelAttribute : PropertyAttribute
     {
         public string Label { get; }
