e2d218594a1fd5e90d66fe736eb7f9493b7a08ff
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2024-11-20 06:49:00 -0400

    Open.Nat added support to Unity
    Fixed a deadlock

--- Core/Dependencies/OpenNat/Finalizer.cs
+++ Core/Dependencies/OpenNat/Finalizer.cs
@@ -23,15 +23,22 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-namespace OpenNat
-{
-	sealed class Finalizer
-	{
-	    ~Finalizer()
-	    {
-	        NatDiscoverer.TraceSource.LogInfo("Closing ports opened in this session");
-	        NatDiscoverer.RenewTimer.Dispose();
-	        NatDiscoverer.ReleaseSessionMappings();
-	    }
-	}
-}
+
+// DON'T WORK IN UNITY.
+// DON'T WORK IN UNITY.
+// DON'T WORK IN UNITY.
+// DON'T WORK IN UNITY.
+// DON'T WORK IN UNITY.
+
+//namespace OpenNat
+//{
+//	sealed class Finalizer
+//	{
+//	    ~Finalizer()
+//	    {
+//	        NatDiscoverer.TraceSource.LogInfo("Closing ports opened in this session");
+//	        NatDiscoverer.RenewTimer.Dispose();
+//	        NatDiscoverer.ReleaseSessionMappings();
+//	    }
+//	}
+//}
