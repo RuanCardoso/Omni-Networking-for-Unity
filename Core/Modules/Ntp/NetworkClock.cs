e619ef214f7b483c0f5bda61682f1c173afb9e76
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2024-12-06 14:28:06 -0400

    Added visual attribute

--- Core/Modules/Ntp/NetworkClock.cs
+++ Core/Modules/Ntp/NetworkClock.cs
@@ -1,41 +1,48 @@
-using Omni.Core.Attributes;
+using TriInspector;
 using UnityEngine;
 
 namespace Omni.Core.Modules.Ntp
 {
-    public class NetworkClock : MonoBehaviour
-    {
-        internal const int DEFAULT_TIME_WINDOW = 60;
-        internal const int DEFAULT_RTT_WINDOW = 25;
-        internal const int DEFAULT_ACCURACY = 5;
-        internal const float DEFAULT_QUERY_INTERVAL = 1f;
+	[DeclareBoxGroup("Samples")]
+	[DeclareBoxGroup("Others")]
+	public class NetworkClock : MonoBehaviour
+	{
+		internal const int DEFAULT_TIME_WINDOW = 60;
+		internal const int DEFAULT_RTT_WINDOW = 25;
+		internal const int DEFAULT_ACCURACY = 5;
+		internal const float DEFAULT_QUERY_INTERVAL = 1f;
 
-        [Header("Samples")]
-        [SerializeField]
-        [Min(1)]
-        private int m_TimeWindow = DEFAULT_TIME_WINDOW;
+		[SerializeField]
 
-        [SerializeField]
-        [Min(1)]
-        private int m_RttWindow = DEFAULT_RTT_WINDOW;
+		[Group("Samples")]
+		[Min(1)]
+		private int m_TimeWindow = DEFAULT_TIME_WINDOW;
 
-        [Header("Others")]
-        [SerializeField]
-        [Range(1, 1000)]
-        private int m_Accuracy = DEFAULT_ACCURACY;
+		[SerializeField]
 
-        [SerializeField]
-        [Label("Query Interval(s)")]
-        [Range(1f, 900f)]
-        private float m_QueryInterval = DEFAULT_QUERY_INTERVAL;
+		[Group("Samples")]
+		[Min(1)]
+		private int m_RttWindow = DEFAULT_RTT_WINDOW;
 
-        [SerializeField]
-        private bool m_UseTickTiming = false;
+		[SerializeField]
+		[Group("Others")]
+		[Range(1, 1000)]
+		private int m_Accuracy = DEFAULT_ACCURACY;
 
-        internal float QueryInterval => m_QueryInterval;
-        internal int TimeWindow => m_TimeWindow;
-        internal int RttWindow => m_RttWindow;
-        internal int Accuracy => m_Accuracy;
-        internal bool UseTickTiming => m_UseTickTiming;
-    }
+		[SerializeField]
+		[LabelText("Query Interval(s)")]
+		[Group("Others")]
+		[Range(1f, 900f)]
+		private float m_QueryInterval = DEFAULT_QUERY_INTERVAL;
+
+		[SerializeField]
+		[Group("Others")]
+		private bool m_UseTickTiming = false;
+
+		internal float QueryInterval => m_QueryInterval;
+		internal int TimeWindow => m_TimeWindow;
+		internal int RttWindow => m_RttWindow;
+		internal int Accuracy => m_Accuracy;
+		internal bool UseTickTiming => m_UseTickTiming;
+	}
 }
