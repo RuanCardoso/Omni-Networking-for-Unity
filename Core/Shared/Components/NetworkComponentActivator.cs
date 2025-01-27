207003528824cc44c02cdaabbfdebdb2039ffc97
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2025-01-27 13:27:25 -0400

    Renamed and polish it

--- Core/Shared/Components/NetworkComponentActivator.cs
+++ Core/Shared/Components/NetworkComponentActivator.cs
@@ -3,12 +3,15 @@ using Omni.Core.Interfaces;
 using UnityEngine;
 
 [DefaultExecutionOrder(-2000)]
-public class NetworkRegisterDisabledComponents : MonoBehaviour
+public class NetworkComponentActivator : MonoBehaviour
 {
     private MonoBehaviour[] components = Array.Empty<MonoBehaviour>();
 
     private void Awake()
     {
+        if (transform != transform.root)
+            throw new Exception("NetworkRegisterDisabledComponents must be on the root object.");
+
         components = transform.GetComponentsInChildren<MonoBehaviour>(true);
         foreach (var component in components)
         {
