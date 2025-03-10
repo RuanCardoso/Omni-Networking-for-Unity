34acaff32f98ee69378cbaac425384c37b113ec4
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2025-03-10 01:40:02 -0400

    refactor: improved layout and styling

--- Core/Components/NetworkBandwidthDisplay.cs
+++ Core/Components/NetworkBandwidthDisplay.cs
@@ -7,93 +7,146 @@ using UnityEngine;
 #pragma warning disable
 public sealed class NetworkBandwidthDisplay : MonoBehaviour
 {
-    private const float m_Padding = 10f;
-    private const float m_ReferenceScreenWidth = 1920f;
-    private const float m_ReferenceScreenHeight = 1080f;
+    #region GUI
+    private const float k_Padding = 10f;
+    private const float k_ReferenceScreenWidth = 1920f;
+    private const float k_ReferenceScreenHeight = 1080f;
+    private const float k_HeaderHeightMultiplier = 1.5f;
+    private const float k_HeaderFontSizeMultiplier = 1.2f;
+    
+    private const int k_StylePadding = 12;
+    private const int k_ContentLeftPadding = 10;
+    private const int k_ContentRightPadding = 20;
 
-    // Client
-    private double clientSentBandwidth;
-    private double clientReceivedBandwidth;
-    private int clientSentPackets;
-    private int clientReceivedPackets;
+    private const float k_LabelYOffsetAddition = 1f;
+    private const float k_DoubleSpacingHeight = 2f; // For the double line spacing in content
 
-    // Server
-    private double serverSentBandwidth;
-    private double serverReceivedBandwidth;
-    private int serverSentPackets;
-    private int serverReceivedPackets;
-
-    // GUI
-    [SerializeField]
     [GroupNext("GUI Settings")]
+    [SerializeField]
     private float m_Width = 310f;
-
-    [SerializeField] private float m_Height = 220f;
-
+    [SerializeField] private float m_Height = 210f;
     [SerializeField] private int m_FontSize = 24;
+    #endregion
+
+    #region Client
+    private double m_ClientOutgoingBandwidth, m_ClientIncomingBandwidth;
+    private int m_ClientOutgoingPacketsPerSecond, m_ClientIncomingPacketsPerSecond;
+    #endregion
+
+    #region Server
+    private double m_ServerOutgoingBandwidth, m_ServerIncomingBandwidth;
+    private int m_ServerOutgoingPacketsPerSecond, m_ServerIncomingPacketsPerSecond;
+    #endregion
 
     private void Start()
     {
-        // Client
+        #region Client
         NetworkManager.ClientSide.SentBandwidth.OnAverageChanged += (avg, pps) =>
         {
-            clientSentBandwidth = avg;
-            clientSentPackets = pps;
+            m_ClientOutgoingBandwidth = avg;
+            m_ClientOutgoingPacketsPerSecond = pps;
         };
 
         NetworkManager.ClientSide.ReceivedBandwidth.OnAverageChanged += (avg, pps) =>
         {
-            clientReceivedBandwidth = avg;
-            clientReceivedPackets = pps;
+            m_ClientIncomingBandwidth = avg;
+            m_ClientIncomingPacketsPerSecond = pps;
         };
+        #endregion
 
-        // Server
+        #region Server
         NetworkManager.ServerSide.SentBandwidth.OnAverageChanged += (avg, pps) =>
         {
-            serverSentBandwidth = avg;
-            serverSentPackets = pps;
+            m_ServerOutgoingBandwidth = avg;
+            m_ServerOutgoingPacketsPerSecond = pps;
         };
 
         NetworkManager.ServerSide.ReceivedBandwidth.OnAverageChanged += (avg, pps) =>
         {
-            serverReceivedBandwidth = avg;
-            serverReceivedPackets = pps;
+            m_ServerIncomingBandwidth = avg;
+            m_ServerIncomingPacketsPerSecond = pps;
         };
+        #endregion
     }
 
 #if OMNI_DEBUG
     private void OnGUI()
     {
-        float widthScale = Screen.width / m_ReferenceScreenWidth;
-        float heightScale = Screen.height / m_ReferenceScreenHeight;
+        float widthScale = Screen.width / k_ReferenceScreenWidth;
+        float heightScale = Screen.height / k_ReferenceScreenHeight;
         float scale = Mathf.Min(widthScale, heightScale);
 
         float scaledWidth = m_Width * scale;
         float scaledHeight = m_Height * scale;
-        float scaledPadding = m_Padding * scale;
+        float scaledPadding = k_Padding * scale;
         int scaledFontSize = Mathf.RoundToInt(m_FontSize * scale);
 
-        GUIStyle style = new(GUI.skin.box)
+        GUIStyle windowStyle = new(GUI.skin.box)
         {
             fontSize = scaledFontSize,
             alignment = TextAnchor.UpperLeft,
-            padding = new RectOffset(Mathf.RoundToInt(12 * scale), 0, Mathf.RoundToInt(12 * scale), 0)
+            padding = new RectOffset(Mathf.RoundToInt(k_StylePadding * scale), 0, Mathf.RoundToInt(k_StylePadding * scale), 0)
         };
+
+        GUIStyle headerStyle = new(GUI.skin.box)
+        {
+            fontSize = Mathf.RoundToInt(scaledFontSize * k_HeaderFontSizeMultiplier),
+            fontStyle = FontStyle.Bold,
+            alignment = TextAnchor.MiddleCenter,
+            normal = { textColor = new Color(0.9f, 0.9f, 1f) }
+        };
+
+        GUIStyle contentStyle = new(GUI.skin.label)
+        {
+            fontSize = scaledFontSize,
+            alignment = TextAnchor.UpperLeft,
+            richText = true,
+            wordWrap = true,
+            normal = { textColor = new Color(0.9f, 0.9f, 0.9f) }
+        };
+
+        string outgoingColorTag = "<color=#8aff8a>";
+        string incomingColorTa = "<color=#8ae1ff>";
 
         if (NetworkManager.IsClientActive)
         {
-            GUI.Box(new(Screen.width - scaledWidth - scaledPadding, scaledPadding, scaledWidth, scaledHeight),
-                $"<b>Local</b>\r\n\r\nSent: {clientSentBandwidth.ToSizeSuffix()}\r\nReceived: {clientReceivedBandwidth.ToSizeSuffix()}\n\nPackets Sent: {clientSentPackets} p/s\nPackets Received: {clientReceivedPackets} p/s",
-                style);
+            Rect windowRect = new(Screen.width - scaledWidth - scaledPadding, scaledPadding, scaledWidth, scaledHeight);
+
+            GUI.BeginGroup(windowRect);
+            GUI.Box(new Rect(0, 0, windowRect.width, windowRect.height), "", windowStyle);
+            GUI.Box(new Rect(0, 0, windowRect.width, scaledFontSize * k_HeaderHeightMultiplier), "CLIENT", headerStyle);
+
+            string clientContent = $"Sent: {outgoingColorTag}{m_ClientOutgoingBandwidth.ToSizeSuffix()}</color>\n" +
+                                  $"Received: {incomingColorTa}{m_ClientIncomingBandwidth.ToSizeSuffix()}</color>\n\n" +
+                                  $"Packets Sent: {outgoingColorTag}{m_ClientOutgoingPacketsPerSecond} p/s</color>\n" +
+                                  $"Packets Received: {incomingColorTa}{m_ClientIncomingPacketsPerSecond} p/s</color>";
+
+            float labelYOffset = scaledFontSize + k_LabelYOffsetAddition;
+            GUI.Label(new Rect(k_ContentLeftPadding, scaledFontSize + labelYOffset, windowRect.width - k_ContentRightPadding, windowRect.height - scaledFontSize * k_HeaderHeightMultiplier - labelYOffset),
+                      clientContent, contentStyle);
+
+            GUI.EndGroup();
         }
 
         if (NetworkManager.IsServerActive)
         {
-            GUI.Box(
-                new(Screen.width - scaledWidth - scaledPadding,
-                    NetworkManager.IsClientActive ? scaledHeight + scaledPadding * 2 : scaledPadding, scaledWidth, scaledHeight),
-                $"<b>Server</b>\r\n\r\nSent: {serverSentBandwidth.ToSizeSuffix()}\r\nReceived: {serverReceivedBandwidth.ToSizeSuffix()}\n\nPackets Sent: {serverSentPackets} p/s\nPackets Received: {serverReceivedPackets} p/s",
-                style);
+            float yPos = NetworkManager.IsClientActive ? scaledHeight + scaledPadding * k_DoubleSpacingHeight : scaledPadding;
+            Rect windowRect = new(Screen.width - scaledWidth - scaledPadding, yPos, scaledWidth, scaledHeight);
+
+            GUI.BeginGroup(windowRect);
+            GUI.Box(new Rect(0, 0, windowRect.width, windowRect.height), "", windowStyle);
+            GUI.Box(new Rect(0, 0, windowRect.width, scaledFontSize * k_HeaderHeightMultiplier), "SERVER", headerStyle);
+
+            string serverContent = $"Sent: {outgoingColorTag}{m_ServerOutgoingBandwidth.ToSizeSuffix()}</color>\n" +
+                                  $"Received: {incomingColorTa}{m_ServerIncomingBandwidth.ToSizeSuffix()}</color>\n\n" +
+                                  $"Packets Sent: {outgoingColorTag}{m_ServerOutgoingPacketsPerSecond} p/s</color>\n" +
+                                  $"Packets Received: {incomingColorTa}{m_ServerIncomingPacketsPerSecond} p/s</color>";
+
+            float labelYOffset = scaledFontSize + k_LabelYOffsetAddition;
+            GUI.Label(new Rect(k_ContentLeftPadding, scaledFontSize + labelYOffset, windowRect.width - k_ContentRightPadding, windowRect.height - scaledFontSize * k_HeaderHeightMultiplier - labelYOffset),
+                      serverContent, contentStyle);
+
+            GUI.EndGroup();
         }
     }
 #endif
