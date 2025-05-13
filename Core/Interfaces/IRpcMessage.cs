9a39bba5af59b1ac1c3d4b2e6cb07966000dcecc
Ruan Cardoso <cardoso.ruan050322@gmail.com>
2025-05-13 16:19:06 -0400

    Update IRpcMessage interface with new RPC methods
    Added new methods and properties to the IRpcMessage interface, including __ServerRpcHandler and methods for setting RPC configurations. These changes enhance the flexibility and functionality of RPC handling.

--- Core/Interfaces/IRpcMessage.cs
+++ Core/Interfaces/IRpcMessage.cs
@@ -1,8 +1,12 @@
+#pragma warning disable
 namespace Omni.Core.Interfaces
 {
     internal interface IRpcMessage
     {
         int IdentityId { get; }
-        void OnRpcInvoked(byte methodId, DataBuffer buffer, NetworkPeer peer, bool isServer, int seqChannel);
+        __RpcHandler<DataBuffer, NetworkPeer, int, __Null__, __Null__> __ServerRpcHandler { get; }
+        void OnRpcReceived(byte rpcId, DataBuffer buffer, NetworkPeer peer, bool isServer, int seqChannel);
+        void SetRpcConfiguration(byte rpcId, NetworkGroup group, bool isServer, byte networkVariableId);
+        void SetRpcConfiguration(byte rpcId, DeliveryMode deliveryMode, Target target, NetworkGroup group, byte sequenceChannel, bool isServer);
     }
 }
