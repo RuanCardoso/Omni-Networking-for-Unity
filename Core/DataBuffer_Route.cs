94480884213298c913d6ed14b4dca0a515723f08
Ruan Cardoso <cardoso.ruan050322@gmail.com>
2025-05-13 16:19:46 -0400

    Update DataBuffer_Route.cs

--- Core/DataBuffer_Route.cs
+++ Core/DataBuffer_Route.cs
@@ -8,21 +8,7 @@ namespace Omni.Core
         internal DeliveryMode DeliveryMode { get; private set; } = DeliveryMode.ReliableOrdered;
         internal RouteTarget Target { get; private set; } = RouteTarget.SelfOnly;
         internal int GroupId { get; private set; }
-        internal DataCache DataCache { get; private set; } = DataCache.None;
         internal byte SequenceChannel { get; private set; }
-
-        /// <summary>
-        /// Sends a message using specified route target and server synchronization settings.
-        /// </summary>
-        /// <param name="target">The destination of the message, such as a specific client or group.</param>
-        /// <param name="options">The server synchronization settings, including delivery mode, group ID, data cache and sequence channel.</param>
-        /// <remarks>
-        /// This overload streamlines message sending by combining several configuration options into one parameter.
-        /// </remarks>
-        public void Send(RouteTarget target, ServerOptions options)
-        {
-            Send(target, options.DeliveryMode, options.GroupId, options.DataCache, options.SequenceChannel);
-        }
 
         /// <summary>
         /// Sends a GET/POST response from the server with specified delivery and target settings.
@@ -36,16 +22,12 @@ namespace Omni.Core
         /// Use this method for fine-grained control over the delivery of server responses.
         /// Defaults are provided for common scenarios, ensuring ease of use for typical use cases.
         /// </remarks>
-        public void Send(RouteTarget target = RouteTarget.SelfOnly,
-            DeliveryMode deliveryMode = DeliveryMode.ReliableOrdered, int groupId = 0, DataCache dataCache = default,
-            byte sequenceChannel = 0)
+        public void Send(RouteTarget target = RouteTarget.SelfOnly, DeliveryMode deliveryMode = DeliveryMode.ReliableOrdered, int groupId = 0, byte sequenceChannel = 0)
         {
-            dataCache ??= DataCache.None;
             SendEnabled = true;
             Target = target;
             DeliveryMode = deliveryMode;
             GroupId = groupId;
-            DataCache = dataCache;
             SequenceChannel = sequenceChannel;
         }
     }
