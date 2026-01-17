2f189d3985321a9512003630120d20b1a3596f5b
Ruan Cardoso <cardoso.ruan050322@gmail.com>
2026-01-17 00:57:23 -0400

    Update preprocessor directive for virtual player check
    Changed the conditional compilation symbol to require UNITY_6000_3_OR_NEWER in addition to OMNI_VIRTUAL_PLAYER_ENABLED and UNITY_EDITOR for the virtual player check in NetworkLogger.

--- Shared/NetworkLogger.cs
+++ Shared/NetworkLogger.cs
@@ -143,7 +143,7 @@ namespace Omni.Shared
                     isClone = true;
                 }
 
-#if OMNI_VIRTUAL_PLAYER_ENABLED && UNITY_EDITOR
+#if OMNI_VIRTUAL_PLAYER_ENABLED && UNITY_6000_3_OR_NEWER && UNITY_EDITOR
                 if (MPPM.IsVirtualPlayer)
                 {
                     isClone = true;
